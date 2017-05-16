9bdd9b5290213599ceeec5909bc89f8c15e0ec67
LMacchi <lau.macchi@gmail.com>
2017-05-16 17:49:49 -0500

    Initial Commit

---
+++ site/profile/manifests/compile_master.pp
@@ -0,0 +1,9 @@
+class profile::compile_master {
+  @@haproxy::balancermember { "haproxy_${::hostname}":
+    listening_service => 'puppet00',
+    ports             => '8140',
+    server_names      => $::hostname,
+    ipaddresses       => $::ipaddress,
+    options           => 'check',
+  }
+}
---
+++ site/profile/manifests/proxy.pp
@@ -0,0 +1,21 @@
+class profile::proxy {
+  class { 'haproxy':
+    defaults_options => {
+      'log'     => 'global',
+      'stats'   => 'enable',
+      'maxconn' => '8000',
+    },
+  }
+  
+  haproxy::listen { 'puppet00':
+    mode      => 'tcp',
+    ipaddress => '0.0.0.0',
+    ports     => '8140',
+    options   => {
+      'option'  => [
+        'tcplog',
+      ],
+      'balance' => 'leastconn',
+    },
+  }
+}
